b3e1ae119b07210db4adbcf2c4526004e53dcc14
jihoon <znftm97@naver.com>
2020-08-11 15:04:21 +0900

    Implement Toggle features

--- src/components/App.js
+++ src/components/App.js
@@ -43,13 +43,40 @@ class App extends Component{
         });
     }
 
+    /* 
+    Item 클릭시 활성화 비활성화, id로 배열 인덱스 찾음
+    findIndex 함수는 특정요소가 배열에서 몇번째 요소인지 찾기 위한 함수
+    매개변수에는 콜백함수이며, true값 반환할때가지 0부터 순차탐색함
+    true 값 반환시 순차탐색 중지하고 해당 요소의 인덱스 반환 
+    */
+    handleToggle = (id) =>{
+        const {todos} = this.state;
+        const index = todos.findIndex(todo => todo.id === id);
+
+        // 찾은 id값을 가진 데이터의 done값을 반전
+        const toggled = {
+            ...todos[index],
+            done: !todos[index].done
+        };
+
+        // slice로 index값 전후 데이터 복사
+        // 그리고 그 사이에 변경된 todo(toggled) 객체 넣어줌
+        this.setState({
+            todos: [
+                ...todos.slice(0,index),
+                toggled,
+                ...todos.slice(index+1, todos.length)
+            ]
+        });
+    }
+
     render(){
         const {input, todos} = this.state; // 비구조화 할당
-        const {handleChange, handleInsert} = this; // 비구조화 할당으로 this.handleChange 에서 this 생략 가능
+        const {handleChange, handleInsert, handleToggle} = this; // 비구조화 할당으로 this.handleChange 에서 this 생략 가능
         return(
             <PageTemplate>
                 <TodoInput onChange={handleChange} onInsert={handleInsert} value={input}/>
-                <TodoList todos={todos}/>
+                <TodoList todos={todos} onToggle={handleToggle}/>
             </PageTemplate>
             
         );
--- src/components/TodoInput/TodoInput.js
+++ src/components/TodoInput/TodoInput.js
@@ -8,8 +8,13 @@ const cx = classNames.bind(styles);
     value = 입력 값
     onChange = input 변경 이벤트
     onInsert = 추가 버튼 클릭 이벤트
+    비구조화 할당 방식의 함수형 컴포넌트
+
+    원래는 const TodoInput = ({props}) =>{} 이런식이라면, 아래에서 props.onChange props.value
+    이런 형태로 되어야 함.
 */ 
 const TodoInput = ({value, onChange, onInsert}) =>{
+
     const handleKeyPress = (e) =>{
         if(e.key === 'Enter'){
             onInsert();
